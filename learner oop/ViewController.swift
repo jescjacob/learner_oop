2321bcf9942452e9edbd0ecf5b52a36f361b3b2e
jessica <chika.workstuff@icloud.com>
2019-05-15 12:16:59 +0700

    Initial Commit

---
+++ learner oop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learner oop
+//
+//  Created by Jessica Jacob on 15/05/19.
+//  Copyright © 2019 Jessica Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
